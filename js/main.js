12e849e924309e9c5904ad35ad7a584e16114291
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2022-04-11 16:34:06 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
 $(function() {
 
-// $('.reviews__slider-item').slick();
+  // fixed header
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
 
+  $(window).on("scroll load resize", function() {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+      if(scrollPos > introH) {
+        header.addClass("fixed");
+      } else {
+        header.removeClass("fixed");
+      }
+  });
+
+  // smooth scroll
+
+  $("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+
+    $("html, body").animate({
+      scrollTop: elementOffset - 70
+    }, 700);
+
+    $('.nav__list').removeClass('open');
+  });
+
+  // mobile menu
+
+  $('.burger-btn').on('click', function(event) {
+    event.preventDefault();
+    $('.nav__list').toggleClass('open');
+  });
+
+  // slider
+
+  $('.reviews__slider').slick({
+    arrows: false,
+    dots: true, 
+    fade: true,
+    autoplay: true
+  });
 })
